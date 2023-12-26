9917adeaab3f52b17806b98c9c52733bd40ef0b2
MonikaGade <mounika10901@gmail.com>
2023-12-27 00:54:28 +0530

    initial-commit

---
+++ logic.js
@@ -0,0 +1,9 @@
+let home=document.querySelector('.home')
+let homepages=['coding.jpg','picture.jpg']
+
+setTimeout(()=>{
+    for(let im of homepages){
+       
+        console.log(document.home.style.backgroundImage);
+    }
+},1000)
